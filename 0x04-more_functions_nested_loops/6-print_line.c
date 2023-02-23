b2483b5416811e9d16404bb121569f6a51818459
0xMaina <bennymmwangi@gmail.com>
2023-02-23 19:13:07 +0000

    Print line in terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,18 @@
 
 void print_line(int n)
 {
-	int a = 0;
-
-	if (n > 0)
+	if (n <= 0)
 	{
-		while (a <= n)
-		{
-			_putchar('_');
-			a++;
-		}
-	 _putchar('\n');
+		_putchar('\n');
 	}
 	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
 	_putchar('\n');
+	}
 }
